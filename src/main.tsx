161cd56fb6ee85bae3e5a6fedfa0e8140dc5b1c2
Bilalbink1 <bilalbink1@gmail.com>
2026-02-18 09:32:51 +0100

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,46 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+import SidebarLayout from "./components/layouts/SidebarLayout";
+import Home from "./pages/Home";
+// import Journey from './pages/Journey';
+// import Projects from './pages/Projects';
+// import ArtsAndCraft from './pages/ArtsAndCraft';
+// import Music from './pages/Music';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <SidebarLayout />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+            // {
+            //   path: 'journey',
+            //   element: <Journey />,
+            // },
+            // {
+            //   path: 'projects',
+            //   element: <Projects />,
+            // },
+            // {
+            //   path: 'arts-and-craft',
+            //   element: <ArtsAndCraft />,
+            // },
+            // {
+            //   path: 'music',
+            //   element: <Music />,
+            // },
+        ],
+    },
+]);
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
+);
